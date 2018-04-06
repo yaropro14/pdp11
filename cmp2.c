7f9739b5472975b546eac6a9c9ab77f906de1167
yaropro14 <yaropro14@gmail.com>
2018-04-07 01:58:21 +0300

    Function (run)

--- cmp2.c
+++ cmp2.c
@@ -1,70 +1,117 @@
 # include <stdio.h>
 # include <assert.h>
+# include <stdlib.h>
+
 
 typedef unsigned char byte;
 typedef unsigned short int word;
 typedef word adr;
 
+
 word mem [64 * 1024];
+
 
 # define LO(x) ((x) & 0xFF)
 # define HI(x) (((x) >> 8) & 0xFF)
 
-byte b_read  (adr a);            // читает из "старой памяти" mem байт с "адресом" a.
-void b_write (adr a, byte val);  // пишет значение val в "старую память" mem в байт с "адресом" a.
-word w_read  (adr a);            // читает из "старой памяти" mem слово с "адресом" a.
-void w_write (adr a, word val);  // пишет значение val в "старую память" mem в слово с "адресом" a.
-void load_file( );
 
-word w_read  (adr a)
+void test_mem ();
+byte b_read  (adr a);            
+void b_write (adr a, byte val);  
+word w_read  (adr a);
+void w_write (adr a, word val);            
+void load_file ( );
+void mem_dump (adr start, word n);
+void run (adr pc0);
+void do_halt ();
+void do_mov ();
+void do_add ();
+void do_unknown();
+
+
+struct Command
+{
+	word opcode;
+	word mask;
+	char * name;
+	void (* func)();
+} commands [] = 
+{
+	{0,			0177777, 	"halt",		do_halt},
+	{0010000,	0170000, 	"mov",		do_mov},
+	{0060000,	0170000, 	"add",		do_add},
+	{0,			0,			"unknown",	do_unknown}
+};
+
+
+void test_mem ()
+{
+    byte b0, b1;
+    word w;
+    b0 = 0x0a;
+    b1 = 0x0b;
+    b_write(2, b0);
+    //printf ("%x", w)
+    b_write(3, b1);
+    w = w_read(2);
+    printf ("%04x = %02x%02x\n", w, b1, b0);
+    assert (w == 0x0a0b);
+    //
+    w = 0x0d0c;
+    w_write (4, w);
+    b0 = b_read (4);
+    b1 = b_read (5);
+    printf ("%04x = %02x%02x\n", w, b1, b0);
+    assert (b0 == 0x0c);
+    assert (b1 == 0x0d);
+}
+
+
+word w_read (adr a)
 {
     return mem[a];
 }
+
 
 void w_write (adr a, word val)
 {
     mem[a] = val;
 }
 
-byte b_read  (adr a)
-{   // w_write (4, 0x0d0c)
+
+byte b_read (adr a)
+{   
     byte res;
     if (a % 2 == 0)
     {
     res = LO(mem[a]);
-    // b_read(4) = 0x0c
     }
     else
     {
-    // b_read(5) = 0x0d
     res = HI (mem[a - 1]);
     a = a - 1;
-    //printf ("mem[%d] = %04x res = %02x HI = %02x\n", a, res, HI(mem[a]));
     }
     return res;
 }
+
 
 void b_write (adr a, byte val)
 {
     word b = 0;
     if (a % 2 == 0)
-    {
+	{
         mem[a] |= val;
-        //printf("%04x\n", mem[a]);
-    }
+	}
     else if (a % 2 != 0)
     {
         b = val;
         b = (b << 8);
         mem[a - 1] = mem[a - 1] | b;
-        //printf ("%04x %04x %04x \n", val, b, mem[a - 1]);
-    }
-    //printf ("%04x\n", mem[a - 1]);
+	}
 }
 
-void mem_dump(adr start, word n);
 
-void load_file( )
+void load_file ( )
 {
     unsigned int a;
     unsigned int b;
@@ -77,15 +124,13 @@ void load_file( )
         {
             //fprintf(stderr, "%d: %s\n", __LINE__, __FUNCTION__);
             fscanf(f,"%x", & val);
-            //fprintf(stderr, "%d: %s\n", __LINE__, __FUNCTION__);
             b_write (i,val);
-            //fprintf(stderr, "%d: %s\n", __LINE__, __FUNCTION__);
         }
-        //fprintf(stderr, "%d: %s\n", __LINE__, __FUNCTION__);
     }
 }
 
-void mem_dump(adr start, word n)
+
+void mem_dump (adr start, word n)
 {
     assert (start % 2 == 0);
     int i = 0;
@@ -95,27 +140,53 @@ void mem_dump(adr start, word n)
     }
 }
 
-void test_mem()
+
+void run (adr pc0)
 {
-    byte b0, b1;
-    word w;
-    b0 = 0x0a;
-    b1 = 0x0b;
-    b_write(2, b0);
-    //printf ("%x", w)
-    b_write(3, b1);
-    w = w_read(2);
-    printf ("%04x = %02x%02x\n", w, b1, b0);
-    /
-    w = 0x0d0c;
-    w_write (4, w);
-    b0 = b_read (4);
-    b1 = b_read (5);
-    printf ("%04x = %02x%02x\n", w, b1, b0);
-    */
-    //assert (b0 == 0x0c);
-    //assert (b1 == 0x0d);
+	adr pc = pc0;
+	int i = 0;
+	while (1)
+	{
+		word w = w_read(pc);
+		//printf ("%06o 5 %06o \n", pc, w);
+		pc += 2;
+		for (i = 0; i <= 3; i++)
+		{
+			struct Command cmd = commands[i];
+			if ((w & cmd.mask) == cmd.opcode)
+			{
+				//printf("%s ", cmd.name);
+				cmd.func();
+			}
+		}
+	}
 }
+
+
+void do_halt ()
+{
+	printf ("halt");
+	exit (0);
+}
+
+
+void do_mov ()
+{
+	printf ("mov");
+}
+
+
+void do_add ()
+{
+	printf ("add");
+}
+
+
+void do_unknown ()
+{
+	printf ("unknown");
+}
+
 
 int main ()
 {
